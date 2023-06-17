0f61d6923cf2d05a019186f939543e13d54d2474
Eche <echyth@yahoo.com>
2023-06-17 17:29:14 +1000

    Express server setup with initia routes

---
+++ routes/auth.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+
+// @route   GET /api/auth
+// @desc   Get logged in user
+// @access  Private
+router.get("/", (req, res) => {
+  res.send("Get logged in user");
+});
+
+// @route   POST /api/auth
+// @desc   Authenticate user and get token
+// @access  Public
+router.post("/", (req, res) => {
+  res.send("Login user");
+});
+
+module.exports = router;
